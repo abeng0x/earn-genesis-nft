bedb8588bf73fabe1d1710ce44ac50f73f0e5afe
earn <earn@abeng0x.local>
2026-05-26 22:20:42 +0700

    fix arc network and free mint

--- src/EarnGenesisNFT.sol
+++ src/EarnGenesisNFT.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyNFT is ERC721URIStorage, Ownable {
+contract EarnGenesisNFT is ERC721URIStorage, Ownable {
 
     uint256 public nextTokenId;
 
